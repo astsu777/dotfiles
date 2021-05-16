0ec47a2accabac3b9112656fc2ee2f304f3003a6
Gaetan <gaetan@ictpourtous.com>
2021-05-17 01:27:10 +0200

    Add scripts

--- local/bin/url_handler.sh
+++ local/bin/url_handler.sh
@@ -1,22 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-# Feed script a url
-# If an image, it will view in feh.
-# If a video or a gif, it will view in mpv
-# If music file or pdf it will download.
-# Otherwise opens link in browser.
+# Feed script a url or file location.
+# If an image, it will view in sxiv,
+# if a video or gif, it will view in mpv
+# if a music file or pdf, it will download,
+# otherwise it opens link in browser.
 
-ext="${1##*.}"
-mpvFiles="mkv mp4 gif avi"
-fehFiles="png jpg jpeg jpe"
-wgetFiles="mp3 flac opus mp3?source=feed pdf shn ogg"
+# If no url given. Opens browser. For using script as $BROWSER.
+[ -z "$1" ] && { "$BROWSER"; exit; }
 
-if echo $fehFiles |grep -w $ext > /dev/null; then
-		nohup feh "$1" > /dev/null &
-elif echo $mpvFiles |grep -w $ext > /dev/null; then
-		nohup mpv --loop --quiet "$1" > /dev/null &
-elif echo $wgetFiles |grep -w $ext > /dev/null; then
-		nohup wget "$1" > /dev/null &
-else
-		nohup $BROWSER "$1" > /dev/null &
-fi
+case "$1" in
+	*mkv|*webm|*mp4|*youtube.com/watch*|*youtube.com/playlist*|*youtu.be*|*hooktube.com*|*bitchute.com*|*videos.lukesmith.xyz*)
+		setsid -f mpv -quiet "$1" >/dev/null 2>&1 ;;
+	*png|*jpg|*jpe|*jpeg|*gif)
+		curl -sL "$1" > "/tmp/$(echo "$1" | sed "s/.*\///;s/%20/ /g")" && sxiv -a "/tmp/$(echo "$1" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
+	*pdf|*cbz|*cbr)
+		curl -sL "$1" > "/tmp/$(echo "$1" | sed "s/.*\///;s/%20/ /g")" && zathura "/tmp/$(echo "$1" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
+	*mp3|*flac|*opus|*mp3?source*)
+		qndl "$1" 'curl -LO'  >/dev/null 2>&1 ;;
+	*)
+		[ -f "$1" ] && setsid -f "$TERMINAL" -e "$EDITOR" "$1" >/dev/null 2>&1 || setsid -f "$BROWSER" "$1" >/dev/null 2>&1
+esac
