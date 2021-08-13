505737c41fc5f3c0356b8e89aaa4694543b3202d
Gaetan <gaetan@ictpourtous.com>
2021-08-13 13:59:15 +0200

    Refresh status bar after refreshing Newsboat

--- config/bspwm/scripts/news.sh
+++ config/bspwm/scripts/news.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if type newsboat > /dev/null 2>&1; then
+	/usr/bin/notify-send "📰 Updating RSS feeds..."
 	news=$(newsboat -x print-unread | awk '{print $1}')
 	icon="📰"
 	printf "%s %s\\n"  "$icon" "$news"
+	/usr/bin/notify-send "📰 RSS feeds updated"
 fi
