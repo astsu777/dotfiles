c3ab7ac5e41106bde028e2614c747d24d5ee0ae6
Gaetan <gaetan@ictpourtous.com>
2021-08-28 11:35:57 +0200

    Set scratchpad size

--- config/i3/scratchpad.sh
+++ config/i3/scratchpad.sh
@@ -2,11 +2,11 @@
 #=========================================================================
 # Author: Gaetan (gaetan@ictpourtous.com) - Twitter: @GaetanICT
 # Creation: Sat 28 Aug 2021 10:43:35
-# Last modified: Sat 28 Aug 2021 11:31:44
+# Last modified: Sat 28 Aug 2021 11:35:44
 # Version: 1.0
 #
 # Description: scratchpad for i3
 #=========================================================================
 if ! pgrep -f 'st -c scratchpad'; then
-	st -c scratchpad
+	st -c scratchpad -g 120x34
 fi
