a611ccd82c6b3db20dae6997e09592e2d17a5841
Gaetan <gaetan@ictpourtous.com>
2021-09-04 16:08:46 +0200

    Add script to display IP address and Internet connectivity

--- config/i3/scripts/internet.sh
+++ config/i3/scripts/internet.sh
@@ -2,21 +2,18 @@
 #=========================================================================
 # Author: Gaetan (gaetan@ictpourtous.com) - Twitter: @GaetanICT
 # Creation: Fri 03 Sep 2021 23:06:10
-# Last modified: Sat 04 Sep 2021 15:40:56
+# Last modified: Sat 04 Sep 2021 16:05:22
 # Version: 1.0
 #
 # Description: shows the current IP address and if connected to Internet or not
-# Show wifi 📶 and percent strength or 📡 if none.
-# Show 🌐 if connected to ethernet or ❎ if none.
-# Show 🔒 if a vpn connection is active
 #=========================================================================
 
+nic=$(ip route get 8.8.8.8 2>/dev/null | sed 's/.*dev \([^ ]*\).*/\1/;t;d')
 ipAddr=$(ip route get 8.8.8.8 2>/dev/null | sed 's/.*src \([^ ]*\).*/\1/;t;d')
-if [ -z "$ipAddr" ]; then ipAddr="-"; fi
-
-case "$(cat /sys/class/net/w*/operstate 2>/dev/null)" in
-	down) wifiicon="📡 " ;;
-	up) wifiicon="$(awk '/^\s*w/ { print "📶", int($3 * 100 / 70) "% " }' /proc/net/wireless)" ;;
+case "$nic" in
+	wl*) icon="📶" ;;
+	*) icon="🌐" ;;
 esac
+if [ -z "$ipAddr" ]; then ipAddr="-" && icon="❎"; fi
 
-printf "%s%s%s %s\n" "$wifiicon" "$(sed "s/down/❎/;s/up/🌐/" /sys/class/net/e*/operstate 2>/dev/null)" "$(sed "s/.*/🔒/" /sys/class/net/tun*/operstate 2>/dev/null)" "$ipAddr"
+printf "%s %s\n" "$icon" "$ipAddr"
