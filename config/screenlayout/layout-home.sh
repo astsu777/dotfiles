1a8455c800d75b90b4c584988ac47d40c95bbcf9
Gaetan <gaetan@ictpourtous.com>
2023-07-26 14:45:35 +0200

    Add two radios from YouTube

--- config/screenlayout/layout-home.sh
+++ config/screenlayout/layout-home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP4-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP3 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output eDP1 --off --output DP4-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP3 --mode 1920x1080 --pos 1920x0 --rotate normal
