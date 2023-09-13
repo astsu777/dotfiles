d3cc7717322e3b9f29fb8a7c5f8708b8e8321809
Gaetan <gaetan@ictpourtous.com>
2023-09-13 09:38:15 +0200

    New office screenlayout script

--- config/screenlayout/layout-home.sh
+++ config/screenlayout/layout-home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP4-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP3 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP3 --off --output DP3-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP3-2 --off --output DP3-3 --off --output DP4 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off
